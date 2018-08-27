c2fb4ca71524b482b46544acb4a3eb3dbaf1716a
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-08-27 15:00:37 +0200

    add initial project for .netcoreapp target framework

---
+++ PackageWithNetCoreAppDependencies/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging.StructuredLogger;
+
+namespace PackageWithNetCoreAppDependencies
+{
+    class Program
+    {
+        public static double plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static void SomeMethodWithLogger()
+        {
+            
+        }
+    }
+}
