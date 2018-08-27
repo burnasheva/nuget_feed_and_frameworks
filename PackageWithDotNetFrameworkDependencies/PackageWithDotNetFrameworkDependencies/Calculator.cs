17188de9c4a0864a7a0c1412beee773a00ebc409
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-08-27 11:15:34 +0200

    initial commit (trying to build it in TeamCity)

---
+++ PackageWithDotNetFrameworkDependencies/PackageWithDotNetFrameworkDependencies/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Logging;
+using Microsoft.Build.Utilities;
+
+namespace PackageWithDotNetFrameworkDependencies
+{
+    public class Calculator
+    {
+        public static double plus(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static void SomeMethodWithLogger()
+        {
+            string binLogFilePath = @"C:\temp\test.binlog";
+
+            var binLogReader = new BinaryLogReplayEventSource();
+            foreach (var record in binLogReader.ReadRecords(binLogFilePath))
+            {
+                var buildEventArgs = record.Args;
+
+                // print command lines of all tool tasks such as Csc
+                if (buildEventArgs is TaskCommandLineEventArgs taskCommandLine)
+                {
+                    Console.WriteLine(taskCommandLine.CommandLine);
+                }
+            }
+        }
+    }
+}
