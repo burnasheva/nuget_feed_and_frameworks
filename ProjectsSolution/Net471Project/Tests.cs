a360ad27493bae66c8b1b4c87f2759026c0b20d3
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-08-27 13:49:39 +0200

    add simple solution for various targeting framework projects

---
+++ ProjectsSolution/Net471Project/Tests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Net471Project
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
