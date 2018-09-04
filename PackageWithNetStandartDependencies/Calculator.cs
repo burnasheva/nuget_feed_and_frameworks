4dad87e27454f0cdb55f1c23fec1eeaca5856644
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-09-04 12:56:18 +0200

    fix package compilation

---
+++ PackageWithNetStandartDependencies/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PackageWithNetStandartDependencies
+{
+    public class Calculator
+    {
+        public static double Plus(double a, double b)
+        {
+            return a + b;
+        }
+    }
+}
--- PackageWithNetStandartDependencies/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PackageWithNetStandartDependencies
-{
-    public class Class1
-    {
-    }
-}
