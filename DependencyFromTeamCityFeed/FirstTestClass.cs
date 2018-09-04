a40bdaaca222a37197ab9a1323e557406c79f6a4
Nadia Burnasheva <nadezhda.burnasheva@gmail.com>
2018-09-04 16:34:21 +0200

    add dependency for Calculator project

---
+++ DependencyFromTeamCityFeed/FirstTestClass.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace DependencyFromTeamCityFeed
+{
+    public class FirstTestClass
+    {
+        [Test]
+        public void TestCalculatorDependency()
+        {
+            Assert.Equals(3.4, Calculator.Calculator.Plus(2.0, 1.4));
+        }
+    }
+}
